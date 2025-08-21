d1ca8c75c453f40b60c3c269cf69b0849af3a688
Simarpreet Kaur <kaurs83@mcmaster.ca>
2025-08-21 12:59:12 -0400

    Debugging

--- multilingualnews.py
+++ multilingualnews.py
@@ -280,4 +280,4 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False, threaded=True)
